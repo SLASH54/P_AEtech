0d83400c04810d3d614394152b26e9d8e658cec7
Aetech <pablotufini@gmail.com>
2025-10-13 10:42:24 -0600

    Fix: Add SSL/TLS configuration for Render deployment

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -75,8 +75,6 @@ exports.loginUser = async (req, res) => {
         res.json({ 
             message: 'Inicio de sesión exitoso.',
             token,
-            nombre,
-            email,
             rol: user.rol,
             usuarioId: user.id
         });
