c3d3db01302487942aee296017c92fc5e0cc1628
Aetech <pablotufini@gmail.com>
2025-10-21 14:53:01 -0600

    Fix: sincronización automática de tablas Evidencias

--- src/models/Evidencia.js
+++ src/models/Evidencia.js
@@ -6,13 +6,17 @@ const { sequelize } = require('../config/database');
 
 
 const Evidencia = sequelize.define('Evidencia', {
-id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-tareaId: { type: DataTypes.INTEGER, allowNull: false },
-usuarioId: { type: DataTypes.INTEGER, allowNull: false },
-titulo: { type: DataTypes.STRING, allowNull: false },
-archivoUrl: { type: DataTypes.STRING, allowNull: false },
-firmaClienteUrl: { type: DataTypes.STRING, allowNull: true },
-}, { tableName: 'Evidencias', timestamps: true });
-
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  tareaId: { type: DataTypes.INTEGER, allowNull: false },
+  usuarioId: { type: DataTypes.INTEGER, allowNull: false },
+  titulo: { type: DataTypes.STRING, allowNull: false },
+  archivoUrl: { type: DataTypes.STRING, allowNull: false },
+  firmaClienteUrl: { type: DataTypes.STRING, allowNull: true },
+}, {
+  tableName: 'Evidencias',
+  timestamps: true,
+});
 
 module.exports = Evidencia;
+
+
