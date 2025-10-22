4f8b841dd9ab254680c8741e5360f9f2aa94f19e
Aetech <pablotufini@gmail.com>
2025-10-22 09:49:47 -0600

    Fix: Estilos del formulario de evidencias

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -2,7 +2,7 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
@@ -14,7 +14,7 @@ const upload = multer({ storage });
 
 const uploadMultiple = upload.array('archivos', 10); // ✅ función
 
-module.exports = { uploadMultiple };
-
+//module.exports = { uploadMultiple };
+module.exports = upload;
 
 
--- src/routes/evidenciaRoutes.js
+++ src/routes/evidenciaRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { protect, rol } = require('../middleware/authMiddleware');
 const { uploadMultiple } = require('../middleware/uploadMiddleware');
 const evidenciaController = require('../controllers/evidenciaController');
-
+const upload = require('../middleware/upload');
 
 const router = express.Router();
 
@@ -26,10 +26,19 @@ module.exports = router;
 router.post(
   '/upload-multiple/:tareaId',
   protect,
-  rol(['Admin', 'Residente', 'Practicante']), // ✅ función que devuelve otra función
-  uploadMultiple, // ✅ middleware multer
-  evidenciaController.subirMultiplesEvidencias // ✅ controlador final
+  rol(['Admin', 'Residente', 'Practicante']),
+  upload.array('archivos', 10), // ✅ esto devuelve una función
+  evidenciaController.subirMultiplesEvidencias
 );
+
+
+//router.post(
+//  '/upload-multiple/:tareaId',
+//  protect,
+//  rol(['Admin', 'Residente', 'Practicante']), // ✅ función que devuelve otra función
+//  uploadMultiple, // ✅ middleware multer
+//  evidenciaController.subirMultiplesEvidencias // ✅ controlador final
+//);
 
 module.exports = router;
 
