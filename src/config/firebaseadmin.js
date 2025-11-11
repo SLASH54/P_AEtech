15153277e502e74d7ce105e6552441eacc6512b6
Aetech <pablotufini@gmail.com>
2025-11-11 12:48:47 -0600

    Fix: Modificaciones Panel de Notificaciones

--- src/config/firebaseadmin.js
+++ src/config/firebaseadmin.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 
 // Archivo de credenciales que descargaste desde Firebase Console
-const serviceAccount = require("../../firebaseServiceAccount.json");
+const serviceAccount = require("../firebaseServiceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
