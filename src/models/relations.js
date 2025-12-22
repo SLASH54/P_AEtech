fadc10288806ca83c7164f965cf9e57ab9c4c761
Aetech <pablotufini@gmail.com>
2025-12-22 11:02:34 -0600

    Fix: guardar levantamientos

--- src/models/relations.js
+++ src/models/relations.js
@@ -6,8 +6,6 @@ const ClienteDireccion = require('./ClienteDireccion');
 const Tarea = require('./Tarea');
 const Evidencia = require('./Evidencia');
 const Notificacion = require('./Notificacion');
-const Levantamiento = require('./Levantamiento')(sequelize, DataTypes);
-
 /* ================= RELACIONES ================= */
 
 // Cliente → Direcciones
