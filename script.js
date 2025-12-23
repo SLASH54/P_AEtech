0ba6d2930bfe635b4e9dbb5801e1d926fec55e0a
Aetech <pablotufini@gmail.com>
2025-12-23 15:22:34 -0600

    Fix: Alias en Panel de tareas

--- script.js
+++ script.js
@@ -1618,7 +1618,7 @@ const clienteMapsLink = clienteMaps
             </td>
             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                <a href="${clienteMapsLink}" target="_blank" class="text-blue-600 hover:underline">
-                  ${textoDireccion} 📍
+                  ${clienteDireccion} 📍
                   ${esMapa ? ' 🗺️' : ''}
                </a>
 
