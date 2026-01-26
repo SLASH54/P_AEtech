843e6f84ed2ef87e755ade0f799592ea11b20e08
Aetech <pablotufini@gmail.com>
2026-01-26 15:54:03 -0600

    Fix: Pagina Cuentas PDF

--- src/controllers/PdfCuentasController.js
+++ src/controllers/PdfCuentasController.js
@@ -4,6 +4,8 @@ const axios = require("axios");
 const sharp = require("sharp");
 
 
+const logoBuf = await cargarImagen(logoURL);
+const logoURL = "https://p-aetech.onrender.com/public/logo.png";
 
 // Función para procesar las imágenes de Cloudinary para el PDF
 async function procesarImagen(url, maxW, maxH) {
@@ -36,6 +38,10 @@ exports.generarPDFCuenta = async (req, res) => {
         doc.pipe(res);
 
         // --- ENCABEZADO ---
+        if (logoBuf) {
+    const logo = doc.openImage(logoBuf);
+    doc.image(logo, 40, 20, { width: 110 });
+  }
         doc.rect(0, 0, 612, 100).fill("#f0f0f0");
         doc.fillColor("#000").fontSize(20).font("Helvetica-Bold").text("NOTA DE SERVICIO", 40, 40);
         doc.fontSize(12).text(cuenta.numeroNota, 40, 65);
