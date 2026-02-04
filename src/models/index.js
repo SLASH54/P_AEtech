4fe939064716dc86472961e61e21dbd0e5d6787e
Aetech <pablotufini@gmail.com>
2026-02-04 14:02:39 -0600

    Fix: Pagina FEA

--- src/models/index.js
+++ src/models/index.js
@@ -2,17 +2,25 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const { sequelize } = require('../config/database');
 
-
+// Importar los modelos
 const LevantamientoModel = require("./Levantamiento");
+const ClienteNegocio = require("./ClienteNegocio");
+const ClienteDireccion = require("./ClienteDireccion");
 
+// Inicializar Levantamiento (porque es una función)
 const Levantamiento = LevantamientoModel(sequelize, DataTypes);
 
+// Definir Relaciones (Opcional pero recomendado)
+ClienteNegocio.hasMany(ClienteDireccion, { foreignKey: 'clienteId' });
+ClienteDireccion.belongsTo(ClienteNegocio, { foreignKey: 'clienteId' });
 
-
+// Exportar TODOS los modelos
 module.exports = {
   sequelize,
   Sequelize,
-  Levantamiento
+  Levantamiento,
+  ClienteNegocio,
+  ClienteDireccion
 };
 
 
