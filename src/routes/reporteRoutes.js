75f77ee1b36e558bc8c27b6fc40071a92c30a249
Aetech <pablotufini@gmail.com>
2026-02-11 12:59:09 -0600

    Fix: imagen pdf

--- src/routes/reporteRoutes.js
+++ src/routes/reporteRoutes.js
@@ -11,6 +11,6 @@ const rolesDescarga = ['Admin', 'Residente', 'Ingeniero'];
 // Ruta para generar y descargar el PDF de una Tarea completada
 router.route('/pdf/:tareaId')
     // El método GET debe devolver el archivo PDF
-    .get(protect, rol(rolesDescarga), reporteController.generateReportePDF);
+    .get(protect, rol(rolesDescarga), reporteController.generarPDFTarea);
 
 module.exports = router;
