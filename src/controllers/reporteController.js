b6044dbbd7b14190d8c6ef4e237cd130ce5ff524
Aetech <pablotufini@gmail.com>
2026-03-17 13:11:48 -0600

    michancla pdf

--- src/controllers/reporteController.js
+++ src/controllers/reporteController.js
@@ -272,7 +272,7 @@ for (let i = 0; i < primerasCuatro.length; i++) {
 
   // CRÍTICO: Si ya dibujamos 2 fotos (la 0 y la 1), bajamos yFotos para la siguiente fila
   if (i === 1) {
-    280 += GAP; // Bajamos el cursor para las imágenes 3 y 4
+    yFotos += GAP; // Bajamos el cursor para las imágenes 3 y 4
   }
 }
 
