58c4ac4e2f91fa2f0ffd964f277832cd70f2e898
Aetech <pablotufini@gmail.com>
2026-04-09 12:29:45 -0600

    firma contratofirma uwu canvas b

--- src/controllers/contratoController.js
+++ src/controllers/contratoController.js
@@ -9,25 +9,25 @@ const cloudinary = require('cloudinary').v2; // 👈 Importamos cloudinary como 
 
 exports.crearContrato = async (req, res) => {
     try {
+        // Recibimos lo que viene del frontend (script.js)
         const { clienteNombre, clienteRFC, contratoFirmaBase64 } = req.body;
 
         const nuevoContrato = await Contrato.create({
-            cliente_nombre: clienteNombre,
-            cliente_rfc: clienteRFC,
-            firma_base64: contratoFirmaBase64 
+            cliente_nombre: clienteNombre, // Coincide con Contrato.js y Neon
+            cliente_rfc: clienteRFC,       // Coincide con Contrato.js y Neon
+            firma_base64: contratoFirmaBase64 // 👈 ESTO debe ser igual al nombre en req.body
         });
 
         res.status(201).json({ 
             success: true, 
-            msg: "✅ Guardado en la tabla contratos", 
+            msg: "✅ Guardado en AE Tech", 
             id: nuevoContrato.id 
         });
     } catch (error) {
         console.error("❌ Error:", error);
-        res.status(500).json({ success: false, msg: "Error de servidor: " + error.message });
+        res.status(500).json({ success: false, msg: error.message });
     }
 };
-
 
 // 🔹 Obtener historial de contratos
 exports.obtenerContratos = async (req, res) => {
