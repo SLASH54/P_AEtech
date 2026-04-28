dcfb07bc9b7a902f9871c01f367c587cc16b2b4a
Aetech <pablotufini@gmail.com>
2026-04-28 12:40:10 -0600

    catalogo cerrar automatico

--- gestion_cuentas/scriptCuentas.js
+++ gestion_cuentas/scriptCuentas.js
@@ -1777,6 +1777,8 @@ function agregarAlPedidoDesdeCatalogo(id) {
         }
 
     } 
+
+
     // --- MODO NUEVA CUENTA ---
     else {
         const nombreReal = producto.nombre || producto.insumo || "Producto sin nombre";
@@ -1803,16 +1805,25 @@ function agregarAlPedidoDesdeCatalogo(id) {
         } else if (typeof levRenderMateriales === "function") {
             levRenderMateriales();
         }
+
+        // 🚀 AGREGA ESTA LÍNEA AQUÍ TAMBIÉN:
+        cerrarModalCatalogo(); 
     }
 }
 
 
-
 function cerrarModalCatalogo() {
     document.getElementById("modalCatalogo").style.display = "none";
-    modoSeleccionCatalogo = false; // Resetear el modo
-    resetFormularioCatalogo(); // Limpiar inputs del catálogo
+    
+    // 👈 ESTA LÍNEA ES LA QUE TE SALVA LA VIDA:
+    // Limpiamos el destino para que "olvide" que estaba editando
+    destinoCatalogo = null; 
+
+    modoSeleccionCatalogo = false; 
+    resetFormularioCatalogo(); 
 }
+
+
 //AQUI TERMINA XDD
 
 
