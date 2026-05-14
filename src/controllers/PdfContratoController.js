c6d6396bfaa8a47a7f3392c61e5719692c7e7f28
Aetech <pablotufini@gmail.com>
2026-05-14 12:04:18 -0600

    contrato

--- src/controllers/PdfContratoController.js
+++ src/controllers/PdfContratoController.js
@@ -71,44 +71,80 @@ exports.generarPDFContrato = async (req, res) => {
         doc.font('Helvetica').text(`Primera. “La prestadora” se obliga a prestar a “la contratante” los servicios especializados monitoreo de sistema de alarma vinculado a central de monitoreo “AE Tech”.`, { align: 'justify' });
         doc.moveDown(0.5);
         
-        doc.text(`Segunda. Los servicios serán proporcionados por “la prestadora” con sus propios elementos personales y materiales. Se obliga a: a) Realizar visitas técnicas. b) Tener acceso a la bitácora de obra.`, { align: 'justify' });
+        doc.text(`. Los servicios objeto del presente contrato serán proporcionados por “la 
+prestadora” a “la contratante” con sus propios elementos personales y materiales, ya que 
+cuenta con el personal especializado necesario para ello y con el equipo idóneo para 
+proporcionarlos. Queda expresamente convenido que los servicios serán prestados por la 
+prestadora a la contratante con su propio personal, obligandose a: a)  
+Realizar con el apoyo total de la contratante las visitas de carácter técnico de cuando 
+menos al inicio y durante el periodo comprendido en el contrato. 
+b) Tener acceso a la bitácora de la obra en ejecución con la autoridad que corresponda para 
+anotar las observaciones pertinentes y que redunden en el proceso constructivo como en 
+el avance de la misma.`, { align: 'justify' });
         doc.moveDown(0.5);
 
-        doc.text(`Tercera. Los suministros en calidad de préstamo (propiedad de la contratante) serán prestados fundamentalmente en las instalaciones solicitadas por la parte contratante, con domicilio en: `, { align: 'justify', continued: true })
+        doc.text(`Tercera. Los servicios y suministros de material y equipo en calidad de préstamo (propiedad de la contratante) objeto del presente contrato serán prestados fundamentalmente en las instalaciones solicitadas por la parte contratante, con domicilio `, { align: 'justify', continued: true })
            .font('Helvetica-Bold').text(`${contrato.domicilio || '_________________________________'}`, { continued: true })
-           .font('Helvetica').text(`, al término del periodo contratado el prestador podrá retirar el equipo.`);
+           .font('Helvetica').text(`, al término del periodo contratado el prestador podrá retirar el equipo  en calidad de préstamo en caso de no renovarse la vigencia del mismo, el personal encargado de prestar los servicios estará sujeto a las reglas sobre el ingreso a las instalaciones.`);
         doc.moveDown(0.5);
 
-        doc.text(`Cuarta. El responsable del proyecto será el Mtro. Dionisio Avila Espinoza, quien llenará la bitácora de equipos integrados.`, { align: 'justify' });
+        doc.text(`Cuarta. Manifiesta “la prestadora” que el responsable de la realización del proyecto objeto de este contrato, será el Mtro. Dionisio Avila Espinoza, quien llenara la bitácora de equipos integrados en el domicilio de la parte contratante. `, { align: 'justify' });
         doc.moveDown(0.5);
 
-        doc.text(`Quinta. La contratante pagará a “la prestadora” la cantidad de $580 (quinientos ochenta 00/100 M.N) el primer día de cada mes durante un periodo de `, { align: 'justify', continued: true })
+        doc.text(`Quinta. Con motivo de los servicios, suministro de materiales y equipo, objeto del presente contrato que proporciona “la prestadora” y que estarán disponibles a favor de “la contratante” durante toda la vigencia del mismo, “la contratante pagara a “la prestadora” la cantidad de $580 (quinientos ochenta 00/100 M.N) el primer día de cada mes durante un periodo de `, { align: 'justify', continued: true })
            .font('Helvetica-Bold').text(`${contrato.meses_contrato || '___'} meses`, { continued: true })
-           .font('Helvetica').text(`, realizando el pago en las oficinas de AE Tech.`);
+           .font('Helvetica').text(`, realizando el pago en las oficinas de AE Tech o mediante transferencia bancaria a los datos proporcionados por AE Tech.`);
         doc.moveDown(0.5);
 
-        doc.text(`Sexta. El contrato es intransferible sin consentimiento por escrito.`, { align: 'justify' });
+        doc.text(`Sexta. Ninguna de las partes contratantes podrá ceder o transmitir los derechos y obligaciones que contraen en los términos del presente contrato sin el expreso consentimiento por escrito de la contraparte respectiva.`, { align: 'justify' });
         doc.moveDown(0.5);
 
         doc.text(`Séptima. El presente contrato tendrá una duración del `, { align: 'justify', continued: true })
            .font('Helvetica-Bold').text(`${contrato.fecha_inicio || '________'}`, { continued: true })
            .font('Helvetica').text(` al `, { continued: true })
            .font('Helvetica-Bold').text(`${contrato.fecha_fin || '________'}`, { continued: true })
-           .font('Helvetica').text(` a partir de la firma del mismo.`);
+           .font('Helvetica').text(` a partir de la firma del mismo, en caso de algún contra tiempo ya sea interno o externo “la prestadora” le notificará a “la contratante” para valorar la situación. .`);
         doc.moveDown(0.5);
 
-        doc.text(`Octava y Novena. Rige como acuerdo único y se somete a la jurisdicción de los tribunales de Atlixco, Puebla.`, { align: 'justify' });
+        doc.text(`Octava.  Las partes manifiestan que el presente contrato es el único que rige la prestación de los servicios objeto del mismo, por lo que dejan sin efecto cualquier otro contrato, convenio, documento o comunicación verbal o escrita que pudiera haber entre las partes. El presente contrato solo podrá ser modificado mediante acuerdo por escrito debidamente firmado. `, { align: 'justify' });
         doc.moveDown(0.5);
 
-        doc.font('Helvetica-Bold').text(`Décima. Penalizaciones.`, { continued: true }).font('Helvetica').text(` Se procederá a finiquitar responsabilidad o retirar equipo si el contratante no proporciona información, no cumple con pagos o no da facilidades físicas.`);
+        doc.text(`Novena.  Para todo lo no previsto en el presente contrato se aplicarán las disposiciones legales previstas en el Código Civil del Estado Libre y Soberano de Puebla vigente. Para cualquier conflicto derivado de la interpretación, cumplimiento o incumplimiento del presente contrato, las partes se someten a la jurisdicción de los tribunales competentes de la ciudad de Atlixco, Puebla, renunciando a cualquier fuero que por razón de su domicilio les corresponda o pudiera corresponderles en el futuro.`, { align: 'justify' });
+        doc.moveDown(0.5);
 
+        doc.font('Helvetica-Bold').text(`Décima.`, { continued: true }).font('Helvetica').text(`Se procederá a las penalizaciones correspondientes de acuerdo a los siguientes puntos: `);
+        doc.moveDown(0.5);
+
+        doc.text(`               - Si el contratante no cumple con sus obligaciones tales como: `, { align: 'justify' });
+        doc.moveDown(0.5);
+        
+        doc.text(`I. No proporcionar la información técnica solicitada por él prestador para su buen desempeño. `, { align: 'justify' });
+        doc.moveDown(0.5);
+
+        doc.text(`II. No cumple con el pago de honorarios por proyecto acordado con el prestador.`, { align: 'justify' });
+        doc.moveDown(0.5);
+
+         doc.text(`III. No proporcionar todas las facilidades físicas a AE Tech para que pueda realizar sus visitas a las obras.`, { align: 'justify' });
+         doc.moveDown(0.5);
+
+         doc.text(`IV. Los montos pueden variar si existen retrasos en la ejecución del proyecto ajenos a AE Tech, ademas de la posibilidad de daños al avance por parte de terceros con o sin dolo. `, { align: 'justify' });
+         doc.moveDown(0.15);
+
+        doc.text(`       - El prestador procedera: `, { align: 'justify' });
+        doc.moveDown(0.5);
+
+        doc.text(` I. AE Tech podrá dar por finiquitado cualquier responsabilidad legal en caso de que el contratante incumpla en alguno de los puntos mencionados. `, { align: 'justify' });
+        doc.moveDown(0.5);
+
+        doc.text(` II. AE Tech podrá hacer el retiro de los equipos en calidad de préstamo en las instalaciones de la parte contratante.  `, { align: 'justify' });
+        doc.moveDown(0.5);
         // --- SECCIÓN DE FIRMAS ---
         // Si después de todo el texto queda muy poco espacio (menos de 200px), agregamos una hoja nueva para las firmas
         if (doc.y > 550) doc.addPage(); 
 
         doc.moveDown(2);
         const hoy = new Date().toLocaleDateString('es-MX', { day: 'numeric', month: 'long', year: 'numeric' });
-        doc.font('Helvetica').text(`Leído el presente y enteradas las partes, lo firman en la ciudad de Atlixco, Puebla, el día ${hoy}.`, { align: 'center' });
+        doc.font('Helvetica').text(`Leído el presente contrato y enteradas las partes  de su contenido, alcance y fuerza legal lo firman de conformidad en la ciudad de Atlixco, Puebla, el día ${hoy}.`, { align: 'center' });
 
         doc.moveDown(6);
         const yFirmas = doc.y;
